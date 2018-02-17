a8c702efad1fb7aeb29217a1a15a1c3bddaeb0d3
FStriewski <fstriewski@outlook.com>
2018-02-17 19:28:02 +0100

    Add select method with regex for email. Focus last 3 chars (domain)

--- main.rb
+++ main.rb
@@ -6,6 +6,11 @@ def read_contacts
 end
 
 json = read_contacts
+json <<   {
+    "name": "Malcom X",
+    "phone": "+1 310 155 8822",
+    "email": "z.org@theroost.ss"
+  }
 output_array = []
 
 # Assignment 1: .map , new hash, name as key, phone as value
@@ -16,4 +21,9 @@ output_array = json.map {|i| Hash[i[:name], i[:phone]] }
 
 # Assignment 2.1: .select , subcollection with phone contain string +1
 output_array = json.select {|i| i[:phone]=~ /\A\+#{1}/  }
+#puts output_array
+
+
+# Assignment 2.2: .select , subcollection with email domain contain org
+output_array = json.select {|i| i[:email][-3..-1]=~ /#{"org"}/  }
 puts output_array
