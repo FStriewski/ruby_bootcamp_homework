3f7a0fe61e02196da998d500af8c4b73eb976672
FStriewski <fstriewski@outlook.com>
2018-02-17 16:59:18 +0100

    Set up I/O variables, add main function (empty)

--- main.rb
+++ main.rb
@@ -4,3 +4,14 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+json = read_contacts
+output_array = []
+
+puts json
+
+# Assignment 1: .map , name as key, phone as value
+
+def array_transformer
+    #
+end
